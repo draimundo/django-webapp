b43824dd48655f8290e9e57fbc177d2265ed824c
Daniel Raimundo <daniel.raimundo@hslu.ch>
2024-02-29 16:50:44 +0100

    add model values

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,33 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=2)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORD = 'RC'
+        CLOTHING = 'CL'
+        POSTER = 'PS'
+        MISCELLANEOUS = 'MS'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        default=2021,
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=5, default=Type.MISCELLANEOUS)
