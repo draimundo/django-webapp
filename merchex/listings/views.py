5cb5b8f57270b8b6a1da71aad9fdb3a0134d0d61
Daniel Raimundo <daniel.raimundo@hslu.ch>
2024-02-29 15:52:47 +0100

    Add new HTML templates for about, contact, hello, and listings pages

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,31 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request, "listings/hello.html", {"bands": bands})
 
 def about(request):
-    return HttpResponse('<h1>About Us</h1><p>Nous adorons merch!</p>')
+    return render(request, "listings/about.html")
 
 def listings(request):
     listings = Listing.objects.all()
-    listinglist = [f"<li>{listing.title}</li>" for listing in listings]
-    listingstr = "<ul>" + "".join(listinglist) + "</ul>"
-    return HttpResponse(f"""
-        <h1>Listings</h1>
-        <p>Liste des produits:</p>
-        {listingstr}
-    """)
+    return render(request, "listings/listings.html", {"listings": listings})
 
 def contact(request):
-    return HttpResponse('<h1>Contact Us</h1><p>Contactez-nous!</p>')
+    return render(request, "listings/contact.html")
