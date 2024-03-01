dfc3e3535561a43df3bf4d059cdfc7be92e02f4d
Daniel Raimundo <daniel.raimundo@hslu.ch>
2024-03-01 09:02:47 +0100

    add band/listing creation, contact form

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.core.mail import send_mail
 from listings.models import Band, Listing
+from listings.forms import BandForm, ContactUsForm, ListingForm
 
 def band_list(request):
     bands = Band.objects.all()
@@ -8,6 +10,16 @@ def band_list(request):
 def band_detail(request, band_id):
     band = get_object_or_404(Band, pk=band_id)
     return render(request, "listings/band_detail.html", {"band": band})
+
+def band_create(request):
+    if request.method == "POST":
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band_id=band.id)
+    else:
+        form = BandForm()
+    return render(request, "listings/band_create.html", {"form": form})
 
 def about(request):
     return render(request, "listings/about.html")
@@ -20,5 +32,31 @@ def listing_detail(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
     return render(request, "listings/listing_detail.html", {"listing": listing})
 
+def listing_create(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing_id=listing.id)
+    else:
+        form = ListingForm()
+    return render(request, "listings/listing_create.html", {"form": form})
+
 def contact(request):
-    return render(request, "listings/contact.html")
+    if request.method == "POST":
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+            return redirect('email-sent')
+    else:
+        form = ContactUsForm()
+
+    return render(request, "listings/contact.html", {"form": form})
+
+def email_sent(request):
+    return render(request, "listings/email_sent.html")
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -21,10 +21,13 @@ from listings import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("bands/", views.band_list, name="band-list"),
-    path('bands/<int:band_id>/', views.band_detail, name='band-detail'),
-    path('about-us/', views.about),
+    path("band/", views.band_list, name="band-list"),
+    path('band/<int:band_id>/', views.band_detail, name='band-detail'),
+    path('band/add/', views.band_create, name='band-create'),
+    path('about-us/', views.about, name='about'),
     path('listing/', views.listing_list, name='listing-list'),
     path('listing/<int:listing_id>/', views.listing_detail, name='listing-detail'),
-    path('contact/', views.contact),
+    path('listing/add/', views.listing_create, name='listing-create'),
+    path('contact-us/', views.contact, name='contact'),
+    path('email-sent/', views.email_sent, name='email-sent'),
 ]
